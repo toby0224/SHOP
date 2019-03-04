ed6cf81b66e6e59ad840cc1ba7ea533d2ecb2b3a
Toby Huynh <tobymhuynh0224@gmail.com>
2019-03-04 15:20:53 -0500

    added code for error handling

--- app.js
+++ app.js
@@ -49,10 +49,16 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        // if no user is found at this time.
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      throw new Error(err); // throw error if no user founder
+    });
 });
 
 app.use((req, res, next) => {
@@ -65,7 +71,14 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get("/500", errorController.get500);
+
 app.use(errorController.get404);
+
+// express error hangling middleware with 4 args
+app.use((error, req, res, next) => {
+  res.redirect("/500");
+});
 
 mongoose
   .connect(MONGODB_URI)
