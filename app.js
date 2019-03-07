bf0571f52c0391a2b7e6c56ad3f5e389b3a163b1
Toby Huynh <tobymhuynh0224@gmail.com>
2019-03-07 17:38:04 -0500

    added payments using stripe

--- app.js
+++ app.js
@@ -10,33 +10,30 @@ const flash = require("connect-flash");
 const multer = require("multer");
 
 const errorController = require("./controllers/error");
-
+const shopController = require("./controllers/shop");
+const isAuth = require("./middleware/is-auth");
 const User = require("./models/user");
 
 const MONGODB_URI =
   "mongodb+srv://tobyhuynh:Daklak0224!!@cluster0-wmxso.mongodb.net/shop";
-
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: "Sessions"
+  collection: "sessions"
 });
-
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images"); // path to save file image
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    // generate name for file image
     cb(null, new Date().toISOString() + "-" + file.originalname);
   }
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    // only accept these type of file images
     file.mimetype === "image/png" ||
     file.mimetype === "image/jpg" ||
     file.mimetype === "image/jpeg"
@@ -57,7 +54,7 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
-); // extract image file from the form
+);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
@@ -68,29 +65,36 @@ app.use(
     store: store
   })
 );
-app.use(csrfProtection);
+
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
       if (!user) {
-        // if no user is found at this time.
         return next();
       }
       req.user = user;
       next();
     })
     .catch(err => {
-      throw new Error(err); // throw error if no user founder
+      next(new Error(err));
     });
 });
 
+app.post("/create-order", isAuth, shopController.postOrder);
+
+app.use(csrfProtection);
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
@@ -103,9 +107,14 @@ app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
-// express error hangling middleware with 4 args
 app.use((error, req, res, next) => {
-  res.redirect("/500");
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
@@ -113,6 +122,6 @@ mongoose
   .then(result => {
     app.listen(5000);
   })
-  .catch(err => console.log(err));
-
-//     "mongodb+srv://tobyhuynh:Daklak0224!!@cluster0-wmxso.mongodb.net/shop?retryWrites=true"
+  .catch(err => {
+    console.log(err);
+  });
